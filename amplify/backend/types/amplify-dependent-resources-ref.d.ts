ef05a4ab0f1004df7c37d99e5f9c005e02ed71c4
ianfmc <desert.digital.us@gmail.com>
2025-03-16 14:13:39 -0700

    Remove change to Add Staff

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -22,6 +22,13 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
+    "getCognitoUsers": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "startworkflow": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
